f05579770eb0807fbb4bbdf4ee2fb2de7707e94b
Sabya Roy <68545445+mr-sabya@users.noreply.github.com>
2026-03-10 00:07:46 +0600

    update database tag fro tag manager

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -35,11 +35,11 @@
     <link rel="stylesheet" href="{{ asset('assets/frontend/css/responsive.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/frontend/css/custom.css') }}">
     @livewireStyles
-    {!! $settings['google_tag_manager(head)'] ?? '' !!}
+    {!! $settings['google_tag_manager(_head)'] ?? '' !!}
 </head>
 
 <body class="default_home">
-    {!! $settings['google_tag_manager(body)'] ?? '' !!}
+    {!! $settings['google_tag_manager(_body)'] ?? '' !!}
 
   
     @include('frontend.partials.preloader')
